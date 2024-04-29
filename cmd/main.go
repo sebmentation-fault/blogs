4fef59236a69dfdeff8bc4884f287499b00a0041
Sebastian Kjällgren <sebastian@kjallgren.com>
2024-04-29 19:38:04 +0100

    feat: follow prime's HTMX/GO video!

--- cmd/main.go
+++ cmd/main.go
@@ -1,50 +1,118 @@
 package main
 
 import (
-	"github.com/donseba/go-htmx"
-	"github.com/donseba/go-htmx/middleware"
-
-	"log"
+	"io"
 	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-type (
-	App struct {
-		htmx *htmx.HTMX
-	}
+type Templates struct {
+    templates *template.Template
+}
 
-	route struct {
-		path    string
-		handler http.Handler
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
-)
+}
+
+type Contact struct {
+	Name, Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name: name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+type Data struct {
+	Contacts Contacts
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("Alice", "alice@gmail.com"),
+			newContact("Bob", "bob@gmail.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	FormData FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		FormData: newFormData(),
+	}
+}
 
 func main() {
-	// new app with htmx instance
-	app := &App{
-		htmx: htmx.New(),
-	}
+	e := echo.New()
+	e.Use(middleware.Logger())
 
-	mux := http.NewServeMux()
+	e.Renderer = newTemplate()
 
-	// wrap routes with the middleware
-	wrapRoutes(mux, middleware.MiddleWare, []route{
-		{path: "/", handler: http.HandlerFunc(app.Home)},
+	page := newPage()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", page)
 	})
 
-	err := http.ListenAndServe(":3210", mux)
-	log.Fatal(err)
-}
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
 
-func (a *App) Home(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "assets/index.html")
-}
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+			return c.Render(422, "form", formData)
+		}
 
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
 
-// wrapRoutes takes a mux, middleware, and a list of routes to apply the middleware to.
-func wrapRoutes(mux *http.ServeMux, mw func(http.Handler) http.Handler, routes []route) {
-	for _, r := range routes {
-		mux.Handle(r.path, mw(r.handler))
-	}
+		c.Render(200, "form", newFormData())
+		return c.Render(http.StatusOK, "oob-contact", contact)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
 }
 
